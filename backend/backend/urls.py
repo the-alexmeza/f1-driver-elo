b8af1355fa03854f4d056deefb27fe6ff1977152
Alex Meza <alex@mezainnovations.com>
2022-02-12 15:02:11 -0600

    Add routes and react app

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from ranking import views
+
+router = routers.DefaultRouter()
+router.register(r'drivers', views.DriverView, 'ranking')
+router.register(r'races', views.RaceView, 'ranking')
+router.register(r'circuits', views.CircuitView, 'ranking')
+router.register(r'deltas', views.EloDeltaView, 'ranking')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
--- backend/ranking/views.py
+++ backend/ranking/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import CircuitSerialize, CircuitSerializer, RaceSerializer, DriverSerializer, EloDeltaSerializer
+from .serializers import CircuitSerializer, RaceSerializer, DriverSerializer, EloDeltaSerializer
 from .models import Race, Circuit, Driver, EloDelta
 
 
 class CircuitView(viewsets.ModelViewSet):
     serializer_class = CircuitSerializer
     queryset = Circuit.objects.all()
+
+
+class DriverView(viewsets.ModelViewSet):
+    serializer_class = DriverSerializer
+    queryset = Driver.objects.all()
+
+
+class RaceView(viewsets.ModelViewSet):
+    serializer_class = RaceSerializer
+    queryset = Race.objects.all()
+
+
+class EloDeltaView(viewsets.ModelViewSet):
+    serializer_class = EloDeltaSerializer
+    queryset = EloDelta.objects.all()
